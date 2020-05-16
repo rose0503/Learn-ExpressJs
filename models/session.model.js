bab897c7290a7c5b2378c40932e5439550686e69
rose0503 <62640435+rose0503@users.noreply.github.com>
2020-05-16 12:09:38 +0700

    Update session.model.js

--- models/session.model.js
+++ models/session.model.js
@@ -1,8 +1,11 @@
 var mongoose = require("mongoose");
 
-var sessionSchema = new mongoose.Schema({
-  cart: { bookId: Number }
-});
+var sessionSchema = new mongoose.Schema(
+  {
+    id: String,
+    cart: [{ bookId: String, count: Number }]
+  }
+);
 
 var Session = mongoose.model("Sessions", sessionSchema, "Sessions");
 module.exports = Session;
